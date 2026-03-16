95a0efec08689194c58ead24589f887c869c4e2b
Marla <Marla.Schevker@gmail.com>
2026-03-15 20:24:12 -0700

    updated character card details

---
+++ src/components/CharacterCard.jsx
@@ -0,0 +1,13 @@
+function CharacterCard({character, isFavorite, onToggleFavorite}) {
+    return (
+        <div>
+            <h3>{character.name}</h3>
+            <p>{character.birth_year}</p>
+            <p>{character.height}</p>
+            <p>{character.gender}</p>
+            <button type="button" onClick={onToggleFavorite}>{isFavorite ? "❤️" : "♡" } </button>
+        </div>
+    )
+}
+
+export default CharacterCard;
